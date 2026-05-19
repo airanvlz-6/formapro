c99230c41012db832bb9102c57258019ca721f50
Airan <airanvlz@email.com>
2026-05-19 11:34:20 +0100

    fix viewport-fit cover

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Forge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
       </head>
       <body>{children}</body>
     </html>
